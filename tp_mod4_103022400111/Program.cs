53674675b2196c837d5ade51e811943620bbf065
m.izzan nur fadhan <129825202+dannskie@users.noreply.github.com>
2026-03-15 21:33:15 +0800

    menambahkan class DoorMachine

--- tp_mod4_103022400111/Program.cs
+++ tp_mod4_103022400111/Program.cs
@@ -26,3 +26,50 @@ public class KodePos
         return tableKodePos[(int)kelurahan];
     }
 }
+
+// bagian doormachine
+public class DoorMachine
+{
+    public enum State { Terkunci, Terbuka };
+
+    private State currentState;
+
+    public DoorMachine()
+    {
+        currentState = State.Terkunci;
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    public void BukaPintu()
+    {
+        currentState = State.Terbuka;
+        Console.WriteLine("Pintu tidak terkunci");
+    }
+
+    public void KunciPintu()
+    {
+        currentState = State.Terkunci;
+        Console.WriteLine("Pintu terkunci");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("=== IMPLEMENTASI KODE POS ===");
+        Kelurahan kelurahan1 = Kelurahan.Batununggal;
+        Console.WriteLine("Kode Pos " + kelurahan1 + " adalah " + KodePos.getKodePos(kelurahan1));
+
+        Kelurahan kelurahan2 = Kelurahan.Maleer;
+        Console.WriteLine("Kode Pos " + kelurahan2 + " adalah " + KodePos.getKodePos(kelurahan2));
+
+        // Simulasi Bagian E
+        Console.WriteLine("\n=== IMPLEMENTASI DOOR MACHINE ===");
+        DoorMachine pintu = new DoorMachine(); 
+        pintu.BukaPintu();                    
+        pintu.BukaPintu();                     
+        pintu.KunciPintu();                    
+        pintu.KunciPintu();                    
+    }
+}
